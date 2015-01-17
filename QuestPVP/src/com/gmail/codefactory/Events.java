9312a25648b99c9c7ecdc85b531df7301a3776e6
Jonas <Jonas@Jonas-PC>
2015-01-17 12:07:50 +0100

    TeleportLoc Klasse hinzugefügt Container für TeleportLocations hinzugefügt Teleports für Schilder werden nun automatisch aus der config.yml gelesen

--- QuestPVP/src/com/gmail/codefactory/Events.java
+++ QuestPVP/src/com/gmail/codefactory/Events.java
@@ -1,7 +1,11 @@
 package com.gmail.codefactory;
 
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public final class Events implements Listener{
@@ -14,6 +18,28 @@ public final class Events implements Listener{
 	@EventHandler
     public void onLogin(PlayerLoginEvent event) {
        System.out.println("Spieler "+event.getPlayer().getName()+" ist dem Server beigetreten!");
+      
     }
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event){
+		 if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+		        if(event.getClickedBlock().getTypeId() == 63 || event.getClickedBlock().getTypeId() == 68) {
 
+		            Sign sign = (Sign) event.getClickedBlock().getState();
+		            String locName = sign.getLine(0);
+		            Location tpLocation = TpLocContainer.getTeleportLoc(locName);
+		            if(tpLocation != null){
+		            	event.getPlayer().teleport(tpLocation);
+		            }else{
+		            	event.getPlayer().sendMessage("Ungültiger Teleport!");
+		            }
+		            //sign.setLine(3, "FubbatPower");
+		            //sign.update();
+
+		            // Do other stuff if you need to
+		        }
+	}
+
+}
 }
--- QuestPVP/src/com/gmail/codefactory/Main.java
+++ QuestPVP/src/com/gmail/codefactory/Main.java
@@ -1,10 +1,37 @@
 package com.gmail.codefactory;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
-    public void onEnable(){ 
+    @SuppressWarnings("unchecked")
+	public void onEnable(){ 
         System.out.println("Unser Subba Dubba Quest PVP ist gestartet!!");
+           
+        //TeleportLocation Container anlegen
+        TpLocContainer.init(getServer().getWorld("world"));
+        List<String> teleports = (List<String>) getConfig().getList("Teleports");
+        Iterator<String> iter = teleports.iterator();
+        
+        StringTokenizer st;
+        String name;
+        double x,y,z;
+        while(iter.hasNext()){
+        	st = new StringTokenizer(iter.next(), ";");
+        	name = st.nextToken();
+        	System.out.println("TpName: "+name);
+        	x = Double.parseDouble(st.nextToken());
+        	y = Double.parseDouble(st.nextToken());
+        	z = Double.parseDouble(st.nextToken());
+        	TpLocContainer.addLoc(name, x, y, z);
+        }
+        
+        
         
       //Methode zum registrieren der Events
         new Events(this);
@@ -13,6 +40,5 @@ public class Main extends JavaPlugin{
     public void onDisable(){ 
        System.out.println("QuestPVP: Peace Out!");
     }
-
 
 }
